9c06de10c2c7ad1026c8d4e90b74cc100abf467e
unknown <lazarmiljakovic1@gmail.com>
2022-03-03 02:26:04 +0100

    m

---
+++ Migrations/20220302235331_V1.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Citaonica.Migrations
+{
+    public partial class V1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fakultet",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Grad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fakultet", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Predmet",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FakultetID = table.Column<int>(type: "int", nullable: true),
+                    Godina = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Predmet", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Predmet_Fakultet_FakultetID",
+                        column: x => x.FakultetID,
+                        principalTable: "Fakultet",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Knjiga",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Godina = table.Column<int>(type: "int", nullable: false),
+                    FakultetID = table.Column<int>(type: "int", nullable: true),
+                    PredmetID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Knjiga", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Knjiga_Fakultet_FakultetID",
+                        column: x => x.FakultetID,
+                        principalTable: "Fakultet",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Knjiga_Predmet_PredmetID",
+                        column: x => x.PredmetID,
+                        principalTable: "Predmet",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skripta",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FakultetID = table.Column<int>(type: "int", nullable: true),
+                    PredmetID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skripta", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Skripta_Fakultet_FakultetID",
+                        column: x => x.FakultetID,
+                        principalTable: "Fakultet",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Skripta_Predmet_PredmetID",
+                        column: x => x.PredmetID,
+                        principalTable: "Predmet",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Knjiga_FakultetID",
+                table: "Knjiga",
+                column: "FakultetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Knjiga_PredmetID",
+                table: "Knjiga",
+                column: "PredmetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Predmet_FakultetID",
+                table: "Predmet",
+                column: "FakultetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skripta_FakultetID",
+                table: "Skripta",
+                column: "FakultetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skripta_PredmetID",
+                table: "Skripta",
+                column: "PredmetID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Knjiga");
+
+            migrationBuilder.DropTable(
+                name: "Skripta");
+
+            migrationBuilder.DropTable(
+                name: "Predmet");
+
+            migrationBuilder.DropTable(
+                name: "Fakultet");
+        }
+    }
+}
