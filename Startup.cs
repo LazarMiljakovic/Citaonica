720b333e1c99d4e4eed6e534e5ec62c924167e5c
unknown <lazarmiljakovic1@gmail.com>
2022-03-08 21:39:14 +0100

    l

--- Startup.cs
+++ Startup.cs
@@ -31,24 +31,6 @@ namespace Citaonica
             services.AddDbContext<CitaonicaContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("CitaonicaCS"));
-
-            });
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CORS",builder =>
-                {
-                    builder.WithOrigins(new string[]
-                    {
-                        "http://localhost:8080",
-                        "https://localhost:8080",
-                        "http://127.0.0.1:8080",
-                        "https://127.0.0.1:8080",
-                    })
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-                    
-                });
             });
 
             services.AddControllers();
@@ -71,8 +53,6 @@ namespace Citaonica
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseCors("CORS");
 
             app.UseAuthorization();
 
